8a10d08edac44595cfdc5c2fc59be073d1378bd7
Gyromitte <gyromitte@gmail.com>
2023-06-30 10:32:40 -0600

    Se agrego una seccion para mostrar la fecha actual En el navbar ahora se muestra el dia mes y año, la fecha se obtiene a traves de la computadora del usuario, mas adelante seria mejor obtener la fecha atraves de otro medio.

--- js/panelAdmin.js
+++ js/panelAdmin.js
@@ -1,13 +1,22 @@
+/*Funcionamiento de la dashboard*/
 // Obtener elementos
 const toggleDashboardBtn = document.getElementById('nav-button');
 const dashboard = document.getElementById('dash-board');
 const main = document.getElementById('main');
-
 // Función para abrir o cerrar el dashboard
 function toggleDashboard() {
   dashboard.classList.toggle('dashboard-open');
   main.classList.toggle('main-dash-open')
 }
-
 // Asignar evento de clic al botón
 toggleDashboardBtn.addEventListener('click', toggleDashboard);
+
+/*Fecha*/
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1; // Los meses en JavaScript comienzan desde 0
+var year = currentDate.getFullYear();
+// Construir la cadena de la fecha (DD/MM/YYYY)
+var formatDate = day + ' / ' + month + ' / ' + year;
+document.getElementById('fecha').innerHTML = formatDate;
+
