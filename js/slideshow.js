5a6d038e12877fb89bbef93ea95baa9a2eff52f5
Gyromitte <gyromitte@gmail.com>
2023-06-29 19:37:16 -0600

    Cambios de resolucion
    Se modificaorn varias reglas CSS para mejorar la visualizacion en resoluciones moviles.

--- js/slideshow.js
+++ js/slideshow.js
@@ -10,5 +10,5 @@ function showSlides() {
   slideIn++;
   if (slideIn > slides.length) {slideIn = 1}
   slides[slideIn-1].style.display = "flex";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Cambiar imagen cada cierto tiempo
 }
