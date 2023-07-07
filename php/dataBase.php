4f565d865d968c3889339d0808909adeb8c92f85
Gyromitte <gyromitte@gmail.com>
2023-07-07 17:16:54 -0600

    Se creo el archivo database
    Este archivo servira para formar una conexion entre la pagina y la base de datos

---
+++ php/dataBase.php
@@ -0,0 +1,59 @@
+<?php
+    class Database
+    {
+        private $PDO_local;
+        private $user = "root";
+        private $password = "";
+        private $server = "mysql:host=localhost; dbname=empresa";
+
+        function conectarBD()
+        {
+            try
+            {
+                $this->PDO_local = new PDO($this->server, $this->user, $this->password);
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage(); 
+            }
+        }
+
+        function desconectarBD()
+        {
+            try
+            {
+                $this->PDO_local = null;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage(); 
+            }
+        }
+
+        function seleccionar($consulta)
+        {
+            try
+            {
+                $resultado = $this->PDO_local->query($consulta);
+                $fila = $resultado->fetchAll(PDO:: FETCH_OBJ);
+                return $fila;
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+
+        function ejecutarSQL($consulta)
+        {
+            try
+            {
+                $this->PDO_local->query($consulta);
+            }
+            catch(PDOException $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+    }
+?>
