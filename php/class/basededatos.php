3c7ce94971ccba7536e9ead99f491d1f388ee1d2
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-07-09 19:42:02 -0600

    conexion entre bd creada

--- php/class/basededatos.php
+++ php/class/basededatos.php
@@ -1,5 +1,25 @@
 <?php
 class BDHotel{
+    private $PDOLocal;
+    private $user="root";
+    private $password="gay";
+    private $server="mysql:host=localhost; dbname=proyecto";
 
+    function conexionHotel(){
+        try{
+            $this->PDOLocal=new PDO($this->server,$this->user,$this->password);
+        }
+        catch(PDOException $E){
+            echo $E->getMessage();
+        }
+    }
+    function desconectarHotel(){
+        try{
+            $this->PDOLocal=null;
+        }
+        catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
 }
 ?>
