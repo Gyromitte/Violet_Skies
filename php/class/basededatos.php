0e4fa2d5277c665ed3b53304648f08a5f8166a26
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-07-14 13:04:45 -0600

    Cambiando el nombre a los columnas de base de datos

--- php/class/basededatos.php
+++ php/class/basededatos.php
@@ -1,9 +1,9 @@
 <?php
 class BDHotel{
     private $PDOLocal;
-    private $user="root";
-    private $password="gay";
-    private $server="mysql:host=localhost; dbname=proyecto";
+    private $user="admin";
+    private $password="admin123";
+    private $server="mysql:host=database-1.cxbyakjiuopg.us-east-1.rds.amazonaws.com; dbname=VIOLET";
 
     function conexionHotel(){
         try{
@@ -44,27 +44,27 @@ class BDHotel{
             }
             if($ver){
                 session_start();
-                if($renglon['TIPO']=='cliente'){
+                if($renglon['TIPO_CUENTA']=='CLIENTE'){
                     $_SESSION["cliente"]=$usu;
                     echo"<div class='alert alert-success'>";
                     echo"<h2 align='center'>Bienvenido ".$_SESSION["cliente"]."</h2>";
                     echo "</div>";
                     header("refresh:2;/html/cliente/index.html");
                 }
-                else{
+                else if($renglon['TIPO_CUENTA']=='ADMINISTRADOR'){
+                    $_SESSION["admin"]=$usu;
+                    echo"<div class='alert alert-success'>";
+                    echo"<h2 align='center'>Bienvenido ".$_SESSION["admin"]."</h2>";
+                    echo "</div>";
+                    header("refresh:2;/html/panelAdmin.html");
+                }
+                else if ($renglon['TIPO_CUENTA']=='EMPLEADO'){
                     $usu=$renglon['id'];
-                    $query="SELECT * FROM USUARIOS JOIN CUENTAS ON CUENTAS.id=USUARIOS.cuentas_id
+                    $query="SELECT * FROM EMPLEADOS JOIN CUENTAS ON CUENTAS.ID=EMPLEADOS.CUENTA
                     WHERE CUENTAS.ID='$usu'";
                     $consulta=$this->PDOLocal->query($query);
                     while($trabajo=$consulta->fetch(PDO::FETCH_ASSOC)){
-                        if($trabajo['tipoUsuario']==''){
-                            $_SESSION["admin"]=$usu;
-                            echo"<div class='alert alert-success'>";
-                            echo"<h2 align='center'>Bienvenido ".$_SESSION["admin"]."</h2>";
-                            echo "</div>";
-                            header("refresh:2;/html/panelAdmin.html");
-                        }
-                        else if($trabajo['tipoUsuario']=='mesero'){
+                        if($trabajo['TIPO']=='MESERO'){
                             $_SESSION["mesero"]=$usu;
                             echo"<div class='alert alert-success'>";
                             echo"<h2 align='center'>Bienvenido ".$_SESSION["mesero"]."</h2>";
@@ -76,7 +76,7 @@ class BDHotel{
                             echo"<div class='alert alert-success'>";
                             echo"<h2 align='center'>Bienvenido ".$_SESSION["chef"]."</h2>";
                             echo "</div>";
-                            header("refresh:2;/html/trabajo.html");
+                            header("refresh:2;/html/cotisazion.html");
                         }
                     }
                 }
