7c88d57e6533c46842e08a7c59a2f89d882d6c9b
Gyromitte <gyromitte@gmail.com>
2023-07-14 19:09:47 -0600

    Se corrigieron consultas en la seccion Empleados
    Ahora la informacion que aparece por encima de las consultas es correcta, ademas se corrigio verMeseros.php para que funcione con la nueva BD

--- js/dinamicTable.js
+++ js/dinamicTable.js
@@ -1,25 +1,38 @@
-/*Dinamic tables*/
-//Conseguir los elementos
+/* Dinamic tables */
+// Conseguir los elementos
 document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.ver-empleados');
-    var tableInfo = document.getElementById('table-info');
-    var contTable = document.querySelector('.cont-table');
-  //Agregar el evento de click a los botones
-    buttons.forEach(function(button) {
+  var buttons = document.querySelectorAll('.ver-empleados');
+  var tableInfo = document.getElementById('table-info');
+  var contTable = document.querySelector('.cont-table');
+  // Agregar el evento de click a los botones
+  buttons.forEach(function(button) {
       button.addEventListener('click', function() {
-        var url = this.getAttribute('data-url');
-        var buttonText = this.textContent;
-  
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState === 4 && xhr.status === 200) {
-            tableInfo.textContent = 'Mostrando ' + buttonText + ':';
-            contTable.innerHTML = xhr.responseText;
-          }
-        };
-        xhr.open('GET', url, true);
-        xhr.send();
+          var url = this.getAttribute('data-url');
+          var buttonId = this.getAttribute('id');
+
+          var xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function() {
+              if (xhr.readyState === 4 && xhr.status === 200) {
+                  var message = getMessageByButtonId(buttonId);
+                  tableInfo.textContent = message;
+                  contTable.innerHTML = xhr.responseText;
+              }
+          };
+          xhr.open('GET', url, true);
+          xhr.send();
       });
-    });
   });
+});
+
+function getMessageByButtonId(buttonId) {
+  switch (buttonId) {
+      case 'verCocineros':
+          return 'Mostrando a los cocineros:';
+      case 'verMeseros':
+          return 'Mostrando a los meseros:';
+      default:
+          return 'Mostrando información:';
+  }
+}
+
   
