6d1b17ad3ec2008ab694765c9422ad87732dbabd
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-07-29 22:18:19 -0600

    Viendo para eso de eventos donde si asiste
    Se me olvido poner eso de no ver si ya estas en ese evento en los otros tipos de ordenes

--- php/viewsEmpleados/panelEmpleado.php
+++ php/viewsEmpleados/panelEmpleado.php
@@ -2,18 +2,19 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!-<![endif]-->
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Violet skies - Panel de Empleado</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--StyleSheets-->
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/panelAdmin.css">
+
     <link rel="icon" type="image/x-icon" href="./images/company_logo.png">
     <!--Referencias a fuentes-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
