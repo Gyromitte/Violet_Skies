b7f86cda3552980c94f173d999c1b0f49dd96f5c
Allixdx <allison.sanroman@outlook.com>
2023-08-01 19:57:27 -0600

    Arreglo búsqueda de empleados

--- php/viewsEmpleados/panelAdmin.php
+++ php/viewsEmpleados/panelAdmin.php
@@ -210,7 +210,7 @@
                 </div>
                 <!--Barra de Busqueda-->
                 <div class="input-group mb-3 search-bar" id="search">
-                    <input type="text" class="form-control" placeholder="Buscar a un empleado" aria-label="" aria-describedby="button-addon2">
+                    <input type="text" id="busqueda" class="form-control" placeholder="Buscar a un empleado" aria-label="" aria-describedby="button-addon2">
                     <button id="buscarEmpleado" data-url="buscarEmpleado.php" class="ver-empleados btn btn-outline-primary" type="button" id="button-addon2">
                     <i class="fa-solid fa-magnifying-glass" style="color: #1f71ff;"></i></button>
                 </div>
