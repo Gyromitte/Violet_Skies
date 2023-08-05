77c79cf08127836f5679b71d42c3991f48153316
Gyromitte <gyromitte@gmail.com>
2023-08-04 22:42:28 -0600

    Verificacion de RFC
    Se han puesto varias medidas para asegurarse que el RFC de los empleados tenga el formato correcto

--- php/viewsEmpleados/guardarEmpleado.php
+++ php/viewsEmpleados/guardarEmpleado.php
@@ -8,37 +8,63 @@ $rfc = $_POST['rfc'];
 $correo = $_POST['correo'];
 $tipo = filter_input(INPUT_POST, 'tipoUsuario', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-// Checar si la cuenta  ya esta registrada en la tabla EMPLEADOS
-$consultaEmpleado = "SELECT * FROM EMPLEADOS WHERE CUENTA IN 
-    (SELECT ID FROM CUENTAS WHERE CORREO = :correo AND TIPO_CUENTA = 'EMPLEADO')";
-$parametrosEmpleado = array(':correo' => $correo);
-$resultadoEmpleado = $db->seleccionarPreparado($consultaEmpleado, $parametrosEmpleado);
+// Variable para almacenar el mensaje de error
+$errorMessage = "";
 
-if ($resultadoEmpleado) {
-    echo "<div class='alert alert-danger'>El empleado ya ha sido dado de alta.</div>";
+// Validar el formato del RFC con una expresión regular
+   // Verificar longitud
+   if (strlen($rfc) < 13) {
+    $errorMessage = "El RFC es demasiado -corto- debe tener exactamente 13 caracteres.";
+} elseif (strlen($rfc) > 13) {
+    $errorMessage = "El RFC es demasiado -largo- debe tener exactamente 13 caracteres.";
 } else {
-    // Si el solicitante no ha sido dado de alta, insertarlo en la tabla EMPLEADOS
-
-    // ID de la cuenta asociada al correo con estado ACTIVO
-    $consultaCuenta = "SELECT ID FROM CUENTAS WHERE CORREO = :correo AND TIPO_CUENTA = 'EMPLEADO' AND ESTADO = 'ACTIVO'";
-    $parametrosCuenta = array(':correo' => $correo);
-    $resultadoCuenta = $db->seleccionarPreparado($consultaCuenta, $parametrosCuenta);
-
-    if ($resultadoCuenta) {
-        $idCuenta = $resultadoCuenta[0]->ID;
-
-        // Insertar el nuevo registro en la tabla EMPLEADOS
-        $cadena = "INSERT INTO EMPLEADOS (RFC, TIPO, CUENTA) VALUES (:rfc, :tipo, :idCuenta)";
-        $parametrosInsercion = array(
-            ':rfc' => $rfc,
-            ':tipo' => $tipo,
-            ':idCuenta' => $idCuenta
-        );
-        $db->ejecutarPreparado($cadena, $parametrosInsercion);
-
-        echo "<div class='alert alert-success'>Empleado Registrado!</div>";
+    // Verificar caracteres inválidos
+    if (!preg_match('/^[A-Z0-9]+$/', $rfc)) {
+        $errorMessage = "El RFC solo puede contener letras mayúsculas y dígitos numéricos.";
     } else {
-        echo "<div class='alert alert-danger'>No se encontró una cuenta de tipo empleado activa con ese correo electrónico.</div>";
+        // Verificar formato
+        if (!preg_match('/^[A-Z]{4}\d{6}[A-Z0-9]{3}$/', $rfc)) {
+            $errorMessage = "El formato del RFC es inválido. Debe ser del tipo AAAA123456XXX, donde AAAA son las primeras cuatro letras del apellido, 123456 representa la fecha de nacimiento (YYMMDD) y XXX es la homoclave.";
+        }
+    }
+}
+
+// Mostrar el mensaje de error si existe
+if (!empty($errorMessage)) {
+    echo "<div class='alert alert-danger'>$errorMessage</div>";
+} else {
+    // Checar si la cuenta  ya esta registrada en la tabla EMPLEADOS
+    $consultaEmpleado = "SELECT * FROM EMPLEADOS WHERE CUENTA IN 
+        (SELECT ID FROM CUENTAS WHERE CORREO = :correo AND TIPO_CUENTA = 'EMPLEADO')";
+    $parametrosEmpleado = array(':correo' => $correo);
+    $resultadoEmpleado = $db->seleccionarPreparado($consultaEmpleado, $parametrosEmpleado);
+
+    if ($resultadoEmpleado) {
+        echo "<div class='alert alert-danger'>El empleado ya ha sido dado de alta.</div>";
+    } else {
+        // Si el solicitante no ha sido dado de alta, insertarlo en la tabla EMPLEADOS
+
+        // ID de la cuenta asociada al correo con estado ACTIVO
+        $consultaCuenta = "SELECT ID FROM CUENTAS WHERE CORREO = :correo AND TIPO_CUENTA = 'EMPLEADO' AND ESTADO = 'ACTIVO'";
+        $parametrosCuenta = array(':correo' => $correo);
+        $resultadoCuenta = $db->seleccionarPreparado($consultaCuenta, $parametrosCuenta);
+
+        if ($resultadoCuenta) {
+            $idCuenta = $resultadoCuenta[0]->ID;
+
+            // Insertar el nuevo registro en la tabla EMPLEADOS
+            $cadena = "INSERT INTO EMPLEADOS (RFC, TIPO, CUENTA) VALUES (:rfc, :tipo, :idCuenta)";
+            $parametrosInsercion = array(
+                ':rfc' => $rfc,
+                ':tipo' => $tipo,
+                ':idCuenta' => $idCuenta
+            );
+            $db->ejecutarPreparado($cadena, $parametrosInsercion);
+
+            echo "<div class='alert alert-success'>Empleado Registrado!</div>";
+        } else {
+            echo "<div class='alert alert-danger'>No se encontró una cuenta de tipo empleado activa con ese correo electrónico.</div>";
+        }
     }
 }
 
