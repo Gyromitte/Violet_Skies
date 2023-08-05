d3d2e7134b073cbef8991cccc623868eac9706f8
Allixdx <allison.sanroman@outlook.com>
2023-08-05 17:23:30 -0600

    Registro de admin sin correo repetido
    Al registrar un nuevo admin se comprueba que la cuenta aún no exista con el correo

--- js/datosAdmin.js
+++ js/datosAdmin.js
@@ -2,15 +2,14 @@
   const btnGuardarCambios = document.getElementById('btnGuardarCambios');
   const btnCancelarCambios = document.getElementById('btnCancelarCambios');
   const successDiv = document.getElementById('alertMessage');
-  // Función para enviar los datos del formulario a cambContraseña.php mediante AJAX
+
   function cambiarContraseña() {
     const formu = document.getElementById('formCambiarContrasena');
       const formCambiarPass = new FormData(formu);
-    // Realizar la solicitud AJAX
+
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
-        // La respuesta del servidor ha sido recibida correctamente
         const response = JSON.parse(this.responseText);
         const alertDiv = document.getElementById('alertMessage');
         if (response.success) {
@@ -22,7 +21,7 @@
 
           setTimeout(function() {
             location.reload();
-          }, 3000); // 3000 milisegundos = 3 segundos
+          }, 3000);
 
         } else {
           alertDiv.classList.add('alert-danger');
@@ -32,7 +31,7 @@
             console.log(response.message);
 
             setTimeout(function() {
-              alertDiv.classList.add('d-none'); // Agrega la clase para ocultar el div
+              alertDiv.classList.add('d-none');
             }, 3000); 
         }
       }
@@ -41,9 +40,8 @@
     xhttp.send(formCambiarPass);
   }
 
-  // Agregar un evento al formulario para llamar a la función cambiarContraseña al enviarlo
   document.getElementById('formCambiarContrasena').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir el envío normal del formulario
+    event.preventDefault();
     cambiarContraseña();
   });
 
@@ -146,34 +144,38 @@
       return false;
     }
     if (!/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/.test(correo)) {
-
       alert("Ingresa una dirección de correo electrónico válida");
       return;
     }
     const mensajeConfirmacion = `¿Seguro que desea registrar un administrador con los siguientes datos?\n\nNombre: ${nombre}\nApellido Paterno: ${ap_paterno}\nApellido Materno: ${ap_materno}\nTeléfono: ${telefono}\nCorreo: ${correo}`;
     if (window.confirm(mensajeConfirmacion)) {
       $.ajax({
-        type: "POST", 
-        url: "../viewsPerfil/registrarAdmin.php", 
+        type: "POST",
+        url: "../viewsPerfil/registrarAdmin.php",
         data: {
           nombre,
           ap_paterno,
           ap_materno,
           telefono,
-          correo
+          correo,
         },
-        success: function () {
-          $("#mensaje").text("Administrador registrado correctamente");
-          $("#mensaje").show();
-          console.log("Se registró exitosamente");
-          setTimeout(function () {
-            $('input[name="nombreNEW"]').val('');
-            $('input[name="ap_paternoNEW"]').val('');
-            $('input[name="ap_maternoNEW"]').val('');
-            $('input[name="telefonoNEW"]').val('');
-            $('input[name="correoNEW"]').val('');
-            $("#mensaje").hide();
-          }, 3000);
+        success: function (response) {
+          const data = JSON.parse(response);
+          if (data.mensaje) {
+
+            alert(`${data.mensaje}. Asegúrate de darle sus credenciales para ingresar a su cuenta\n\nCorreo: ${data.correo}\nContraseña: ${data.contraseña}`);
+            console.log("Se registró exitosamente");
+            setTimeout(function () {
+              $('input[name="nombreNEW"]').val("");
+              $('input[name="ap_paternoNEW"]').val("");
+              $('input[name="ap_maternoNEW"]').val("");
+              $('input[name="telefonoNEW"]').val("");
+              $('input[name="correoNEW"]').val("");
+            }, 3000);
+          } else {
+            alert(data);
+            console.error("Error al registrar nuevo administrador:", data);
+          }
         },
         error: function (error) {
           $("#mensaje").text("Error al registrar nuevo usuario");
@@ -182,10 +184,11 @@
             $("#mensaje").hide();
           }, 3000);
           console.error("Error al enviar los datos:", error);
-        }
+        },
       });
     }
   }
+  
   $(document).ready(function () {
-    $('#registrarAdmin').click(nuevoAdmin);
+    $("#registrarAdmin").click(nuevoAdmin);
   });
--- php/viewsPerfil/registrarAdmin.php
+++ php/viewsPerfil/registrarAdmin.php
@@ -7,22 +7,42 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $telefono = $_POST["telefono"];
     $correo = $_POST["correo"];
 
-    $contraseña = $nombre . rand(1000, 99999);
-    $passHash = password_hash($contraseña, PASSWORD_DEFAULT);
-
     $conexion = new Database();
     $conexion->conectarBD();
 
-    $sql = "INSERT INTO CUENTAS (NOMBRE, AP_PATERNO, AP_MATERNO, TELEFONO, CORREO, CONTRASEÑA, TIPO_CUENTA) 
-    VALUES ('$nombre', '$ap_paterno', '$ap_materno', '$telefono', '$correo', '$passHash', 'ADMINISTRADOR')";
-
-    $resultado = $conexion->ejecutarSQL($sql);
-    if ($resultado === TRUE) {
-        echo "El administrador se registró correctamente";
-    } else {
-        echo "Error al registrar al administrador" . mysqli_error();
+    $consulta = "SELECT COUNT(*) as cuenta FROM CUENTAS WHERE CORREO = '$correo'";
+    $resultadoConsulta = $conexion->seleccionar($consulta);
+    if (!$resultadoConsulta) {
+        $response = array("mensaje" => "Error al verificar el correo");
+        echo json_encode($response);
+        exit; // Detener el script
     }
 
+    $fila = $resultadoConsulta[0];
+
+    if (is_object($fila)) {
+        $fila = (array) $fila;
+    }
+    
+    if ($fila["cuenta"] > 0) {
+        $response = array("Ya existe una cuenta con el mismo correo");
+        echo json_encode($response);
+    } else {
+        $contraseña = $nombre . rand(1000, 99999);
+        $passHash = password_hash($contraseña, PASSWORD_DEFAULT);
+    
+        $sql = "INSERT INTO CUENTAS (NOMBRE, AP_PATERNO, AP_MATERNO, TELEFONO, CORREO, CONTRASEÑA, TIPO_CUENTA) 
+        VALUES ('$nombre', '$ap_paterno', '$ap_materno', '$telefono', '$correo', '$passHash', 'ADMINISTRADOR')";
+    
+        $resultado = $conexion->ejecutarSQL($sql);
+        if ($resultado !== FALSE) {
+            $response = array("mensaje" => "El administrador se registró correctamente", "correo" => $correo, "contraseña" => $contraseña);
+            echo json_encode($response);
+        } else {
+            $response = array("mensaje" => "Error al registrar al administrador");
+            echo json_encode($response);
+        }
+    }
     $conexion->desconectarBD();
 }
 ?>
