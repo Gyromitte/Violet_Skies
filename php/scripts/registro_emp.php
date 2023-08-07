60e054831af49f434dc6664b131daf58ca75dc83
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-08-07 00:55:41 -0600

    Cambio de eso de espanol y no puedes poner comillas en numero de celular

--- php/scripts/registro_emp.php
+++ php/scripts/registro_emp.php
@@ -16,28 +16,34 @@
     // Use a regular expression to check if the phone number contains any letter
     if (preg_match("/[a-zA-Z]/", $cel)) {
         // If the phone number contains a letter, it is invalid
-        echo "<div class='alert alert-danger'>Error: No utilizar letras en tu numero de Celular</div>";
+        echo "<div class='alert alert-danger'>No utilizar letras en tu numero de Celular</div>";
     }
     else if (preg_match("$pattern", $nom)) {
         // The input text contains numbers or special characters
-        echo"<div class='alert alert-danger'>Error: No poner numeros o caracteres especiales en 
+        echo"<div class='alert alert-danger'>No poner numeros o caracteres especiales en 
         los nombres</div>";
     }
     else if (preg_match("$pattern", $ap)) {
         // The input text contains numbers or special characters
-        echo"<div class='alert alert-danger'>Error: No poner numeros o caracteres especiales en 
+        echo"<div class='alert alert-danger'>No poner numeros o caracteres especiales en 
         los nombres</div>";
     }
     else if (preg_match("$pattern", $am)) {
         // The input text contains numbers or special characters
-        echo"<div class='alert alert-danger'>Error: No poner numeros o caracteres especiales en 
+        echo"<div class='alert alert-danger'>No poner numeros o caracteres especiales en 
         los nombres</div>";
     }
-    else if(strlen($cel) !== 10){
-        echo "<div class='alert alert-danger'>Error: Numero de celular de 10</div>";
+    else if(strlen($cel) < 10){
+        echo "<div class='alert alert-danger'>Numero de celular demasiado corto</div>";
+    }
+    else if(strlen($cel) > 10){
+        echo "<div class='alert alert-danger'>Numero de celular demasiado grande</div>";
+    }
+    else if(preg_match('/^\d+$/', $cel)){
+        echo "<div class='alert alert-danger'>No utilizar caracteres especiales</div>";
     }
     else if($nom=="" || $ap==""||$am==""||$usu==""||$pass==""||$confirm==""||$cel==""){
-        echo "<div class='alert alert-danger'>Error: Registros vacios, favor de llenar</div>";
+        echo "<div class='alert alert-danger'>Registros vacios, favor de llenar</div>";
     }
     else{
         $db->Register($nom,$ap,$am,$usu,$pass,$confirm,$cel,$tipo);
