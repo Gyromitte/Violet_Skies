a09c4a3bbe6d8b54de032064c3fa79bf2b244753
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-08-07 18:07:21 -0600

    Ya se corrigio el bug del registro con eso del numero de celular

--- php/scripts/registrocliente.php
+++ php/scripts/registrocliente.php
@@ -42,8 +42,9 @@ error_reporting(E_ALL);
     else if(strlen($cel) > 15){
         echo "<div class='alert alert-danger'>Numero de celular demasiado grande</div>";
     }
-    else if(preg_match('/^\d+$/', $cel)){
-        echo "<div class='alert alert-danger'>No utilizar caracteres especiales</div>";
+    else if(!ctype_digit($cel)){
+        echo "<div class='alert alert-danger'>No utilizar caracteres especiales dentro del numero
+        de celular</div>";
     }
     else if($nom=="" || $ap==""||$am==""||$usu==""||$pass==""||$confirm==""||$cel==""){
         echo "<div class='alert alert-danger'>Registros vacios, favor de llenar</div>";
