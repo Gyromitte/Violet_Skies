b932e6cbc8c3e08bc7311b187d1d09be7ddd0496
Gyromitte <75540620+Gyromitte@users.noreply.github.com>
2023-08-13 11:33:47 -0600

    Dropdown para re-incorporar
    Ahora se genera un solo boton de re-icorporar cuando el menu es de tipo descontinuado

--- php/viewsMenus/verMenus.php
+++ php/viewsMenus/verMenus.php
@@ -5,6 +5,8 @@ $conexion->conectarBD();
 
 isset($_GET['vista']);
     $vista = $_GET['vista'];
+
+
     
     $consulta = "SELECT c.ID, c.NOMBRE, c.DESCRIPCION, tc.TIPO
                  FROM COMIDAS c
@@ -30,22 +32,40 @@ foreach ($tabla as $registro) {
     echo "<td> $registro->NOMBRE </td>";
     echo "<td> $registro->DESCRIPCION </td>";
     echo "<td> $registro->TIPO </td>";
-    // Generar el botón de opciones con el menú desplegable
-    echo "<td class='text-center'>";
-    echo '<div class="dropdown">';
-    echo '<button class="btn btn-secondary dropdown-toggle custom-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">';
-    echo '</button>';
-    echo '<ul class="dropdown-menu custom-drop-menu">';
-    echo '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#mainModal" data-bs-whatever="@editarMenu" 
-    data-id="' . $registro->ID . '">
-    <i class="fa-solid fa-pencil me-2" style="color: #ffffff;"></i>Editar</a></li>';
-    echo '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#mainModal" data-bs-whatever="@descontinuarMenu" 
-    data-id="' . $registro->ID . '">
-    <i class="fa-solid fa-circle-minus me-2" style="color: #ffffff;"></i>Descontinuar</a></li>';
-    echo '</ul>';
-    echo '</div>';
-    echo "</td>";
-    echo '</tr>';
+
+    //8 es DESCONTINUADO
+    if($vista == '8'){
+        echo "<td class='text-center'>";
+        echo '<div class="dropdown">';
+        echo '<button class="btn btn-secondary dropdown-toggle custom-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">';
+        echo '</button>';
+        echo '<ul class="dropdown-menu custom-drop-menu">';
+        echo '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#mainModal" data-bs-whatever="@editarMenu" 
+        data-id="' . $registro->ID . '">
+        <i class="fa-solid fa-rotate-left me-2" style="color: #ffffff;"></i>Re-Incorporar</a></li>';
+        echo '</ul>';
+        echo '</div>';
+        echo "</td>";
+        echo '</tr>';
+    }else{
+        // Generar el botón de opciones con el menú desplegable
+        echo "<td class='text-center'>";
+        echo '<div class="dropdown">';
+        echo '<button class="btn btn-secondary dropdown-toggle custom-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">';
+        echo '</button>';
+        echo '<ul class="dropdown-menu custom-drop-menu">';
+        echo '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#mainModal" data-bs-whatever="@editarMenu" 
+        data-id="' . $registro->ID . '">
+        <i class="fa-solid fa-pencil me-2" style="color: #ffffff;"></i>Editar</a></li>';
+        echo '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#mainModal" data-bs-whatever="@descontinuarMenu" 
+        data-id="' . $registro->ID . '">
+        <i class="fa-solid fa-circle-minus me-2" style="color: #ffffff;"></i>Descontinuar</a></li>';
+        echo '</ul>';
+        echo '</div>';
+        echo "</td>";
+        echo '</tr>';
+    }
+
     }
     
     echo '</tbody>';
