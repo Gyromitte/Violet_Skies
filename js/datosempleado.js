8225a8837746fe28e8c229685a4cc75885059251
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-08-13 18:03:57 -0600

    se arreglo el mensaje
    el mensaje volvio a aparecer en donde devia

--- js/datosempleado.js
+++ js/datosempleado.js
@@ -1,7 +1,7 @@
 const btnEditarDatosPersonales = document.getElementById('btnEditarDatosPersonales');
 const btnGuardarCambios = document.getElementById('btnGuardarCambios');
 const btnCancelarCambios = document.getElementById('btnCancelarCambios');
-const successDiv = document.getElementById('mensajeModificar');
+const successDiv = document.getElementById('alertMessage');
   
 function cambiarContraseña() {
     const formu = document.getElementById('formCambiarContrasena');
@@ -45,3 +45,95 @@ function cambiarContraseña() {
     cambiarContraseña();
   });
  
+  function habilitarEdicion() {
+    const inputs = document.querySelectorAll('.personal-info input[disabled]');
+    const telefonoError = document.getElementById('telefonoError'); 
+
+    inputs.forEach(input => {
+        if (input.name === 'telefono') { 
+            input.removeAttribute('disabled');
+            telefonoError.textContent = ''; // Clear the error message
+        }
+    });
+
+    btnEditarDatosPersonales.classList.add('d-none');
+    btnGuardarCambios.classList.remove('d-none');
+    btnCancelarCambios.classList.remove('d-none');
+}
+
+
+function guardarCambios() {
+  const inputs = document.querySelectorAll('.personal-info input:not([disabled])');
+  const newData = {};
+
+  inputs.forEach(input => {
+      newData[input.name] = input.value;
+  });
+  
+  const telefonoValue = newData['telefono'];
+  const telefonoPattern = /^[0-9]{10}$/;
+
+  if (!telefonoPattern.test(telefonoValue)) {
+      const telefonoError = document.getElementById('telefonoError');
+      telefonoError.textContent = "Ingresa un número de teléfono válido (10 dígitos numéricos)";
+
+      setTimeout(function () {
+          telefonoError.textContent = '';
+      }, 4000);
+
+      return false;
+  }
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          if (response.success) {
+              const telefonoSuccess = document.getElementById('telefonoSuccess');
+              telefonoSuccess.style.display = "block";
+telefonoSuccess.innerHTML = "Teléfono guardado correctamente.";
+setTimeout(function() {
+    telefonoSuccess.style.display = "none";
+}, 4000);
+
+// Get the top position of the perfilSection
+var perfilSectionTop = document.getElementById("perfilSection").offsetTop;
+
+// Reload the page after a brief delay
+setTimeout(function() {
+    window.scrollTo(0, perfilSectionTop); // Scroll to the perfilSection
+    location.reload();
+}, 4000);
+              inputs.forEach(input => {
+                  input.setAttribute('disabled', 'true');
+              });
+          } else {
+                successDiv.textContent = "Error al intentar hacer los cambios";
+                successDiv.classList.remove('d-none');
+                setTimeout(function () {
+                    successDiv.classList.add('d-none');
+                }, 3000);
+            }
+
+            btnEditarDatosPersonales.classList.remove('d-none');
+            btnGuardarCambios.classList.add('d-none');
+            btnCancelarCambios.classList.add('d-none');
+        }
+    };
+    xhttp.open("POST", "../viewsPerfil/guardarDatos.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("data=" + JSON.stringify(newData));
+}
+
+
+function cancelarCambios() {
+    const inputs = document.querySelectorAll('.personal-info input:not([disabled])');
+    inputs.forEach(input => {
+        input.setAttribute('disabled', 'true');
+        input.value = input.defaultValue;
+    });
+
+    btnEditarDatosPersonales.classList.remove('d-none');
+    btnGuardarCambios.classList.add('d-none');
+    btnCancelarCambios.classList.add('d-none');
+}
