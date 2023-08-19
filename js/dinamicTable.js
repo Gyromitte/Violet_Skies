34307a4a4a681edf971965aff397fd0d5ef7fbe4
Gyromitte <75540620+Gyromitte@users.noreply.github.com>
2023-08-19 16:16:53 -0600

    Opciones para clientes

--- js/dinamicTable.js
+++ js/dinamicTable.js
@@ -175,6 +175,91 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+//Funcion para las vistas de clientes
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.querySelectorAll('.ver-clientes');
+  var tableInfo = document.getElementById('table-info');
+  var contTable = document.getElementById('cont-table');
+
+  var busquedaInput = document.getElementById('busqueda');
+  // Agregar el evento de click a los botones
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var url = '/php/viewsAdminCli/verClientes.php';
+      var buttonId = this.getAttribute('id');
+      var vista = this.getAttribute('data-vista');
+
+      if (buttonId === 'buscarEmpleado') {
+        var busquedaValor = busquedaInput.value.trim(); // Obtener el valor de la barra de búsqueda y eliminar los espacios en blanco al inicio y al final
+
+        if (busquedaValor === '') {
+          ('La barra de búsqueda está vacía. Por favor, ingrese un correo o nombre');
+          return; // Detener la ejecución si la barra de búsqueda está vacía
+        }
+        // Realizar la búsqueda con el valor ingresado en la barra de búsqueda
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            var message = getMessageByButtonId(buttonId);
+            tableInfo.innerHTML = message; // Modificar el innerHTML en lugar de textContent
+            contTable.innerHTML = xhr.responseText;
+            // Limpiar la barra de búsqueda después de una búsqueda exitosa
+            //Bug donde al querer eliminar o editar el warning salia por que la barra estaba vacia
+            //busquedaInput.value = '';
+          }
+        };
+        xhr.open('GET', "buscarEmpleado.php?busqueda=" + busquedaValor, true);
+        xhr.send();
+      } else {
+        if(buttonId == "verGraficosMenus")
+        { 
+          contTable.innerHTML = '';
+          tableInfo.innerHTML = '';
+          //Re-insertar el grafico
+          contTable.innerHTML = `
+          <div id="cont-table-menus">
+          <!--Contenido Default-->
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- Contenido de la izquierda -->
+                    <div class="col-md-5 pieMenus">
+                        <canvas id="pieTipoMenus" style="height: 60%; width: 100%; margin-right: 0px !important;"></canvas>
+                    </div>
+                    <!-- Canvas a la derecha -->
+                    <div class="info-card col-md-7" style="margin-right: 0px !important;">
+                        <canvas id="menuChart2" style="height: 70%; width: 100%;"></canvas>
+                    </div>
+                </div>
+            </div>
+          </div>
+          `;
+          //Volver a ejecutar el codigo de la grafica para actualizar datos:
+          recargarGraficos(); //Llamado desde chart.js
+          // Manejar el redimensionamiento del canvas
+          const canvas = document.getElementById("proporcionEmpleados2");
+          if (canvas) {
+            const context = canvas.getContext("2d");
+            context.canvas.width = canvas.offsetWidth;
+            context.canvas.height = canvas.offsetHeight;
+          }
+          tableInfo.innerHTML = '';
+        }else{
+          var xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+              var message = getMessageByButtonId(buttonId);
+              tableInfo.innerHTML = message; // Modificar el innerHTML en lugar de textContent
+              contTable.innerHTML = xhr.responseText;
+            }
+          };
+          xhr.open('GET', url + '?vista=' + vista, true);
+          xhr.send();
+        }
+      }
+    });
+  });
+});
+
 
 //Cambiar el mensaje de información
 function getMessageByButtonId(buttonId) {
@@ -191,6 +276,15 @@ function getMessageByButtonId(buttonId) {
     case 'verSolicitudes':
       currentTable = 'solicitud';
       return 'Éstas son tus solicitudes pendientes: <i class="fa-solid fa-business-time" style="color: #343434;"></i>';
+    case 'verEve3':
+      currentTable = '';
+      return 'Clientes con 3 o mas eventos a su nombre <i class="fa-solid fa-users style="color: #343434;"></i>';
+    case 'verEve1':
+      currentTable = '';
+      return 'Clientes con 1-2 o mas eventos a su nombre <i class="fa-solid fa-users style="color: #343434;"></i>';
+    case 'verSinPrece':
+      currentTable = '';
+      return 'Clientes sin precedentes <i class="fa-solid fa-users style="color: #343434;"></i>';   
     case 'verBebidas':
       currentTable = 'bebidas';
       return 'Menús del tipo bebidas: <i class="fa-solid fa-wine-glass" style="color: #343434;"></i>';
