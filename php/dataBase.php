ecaa33ba94acb7126f5b682359adc68e4b2bd664
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-08-19 11:52:16 -0600

    Session a sesion

--- php/dataBase.php
+++ php/dataBase.php
@@ -196,7 +196,7 @@
             session_start();
             session_destroy();
             echo"<div class='container'>";
-                    echo"<h1 align='center'>Cerrando Session</h1>";
+                    echo"<h1 align='center'>Cerrando Sesion</h1>";
                     echo"</div>";
             header("refresh:3;/index.html");
         }
